0dd2b47cafc305de7e394e028991e605a7579aee
Heahmund <tima260399@mail.ru>
2022-12-22 12:45:33 +0200

    Fix6. Issue #8355

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  Shop::Application.configure do
+  Shopf::Application.configure do
     config.action_mailer.delivery_method = :test
     end
 end
